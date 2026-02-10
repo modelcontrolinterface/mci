c0f03e399a884dc7b356e201f414991109477098
0x15BA88FF <86390213+0x15BA88FF@users.noreply.github.com>
2026-02-10 08:19:39 -0800

    feat: add digest to db model
    - Add digest field for selection, creation and updates
    - Add digest validation

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,39 @@
 use crate::{
     schema::definitions,
-    utils::regex::{NAMESPACE_ID_REGEX, TYPE_IDENTIFIER_REGEX},
+    utils::regex::{NAMESPACE_ID_REGEX, TYPE_IDENTIFIER_REGEX, SHA256_REGEX},
 };
+use std::borrow::Cow;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use validator::{Validate, ValidationError};
+
+fn validate_digest(digest: &str) -> Result<(), ValidationError> {
+    let (algorithm, hash) = digest
+        .split_once(':')
+        .ok_or_else(|| {
+            let mut error = ValidationError::new("invalid_digest_format");
+            error.add_param(Cow::from("value"), &digest);
+            error
+        })?;
+    let hash_regex = match algorithm {
+        "sha256" => &SHA256_REGEX,
+        _ => {
+            let mut error = ValidationError::new("unsupported_digest_algorithm");
+            error.add_param(Cow::from("value"), &digest);
+            error.add_param(Cow::from("algorithm"), &algorithm);
+            return Err(error);
+        }
+    };
+
+    if hash_regex.is_match(hash) {
+        Ok(())
+    } else {
+        let mut error = ValidationError::new("invalid_hash_format");
+        error.add_param(Cow::from("value"), &digest);
+        error.add_param(Cow::from("algorithm"), &algorithm);
+        Err(error)
+    }
+}
 
 #[derive(Queryable, Selectable, Serialize)]
 #[diesel(table_name = definitions)]
@@ -16,6 +45,7 @@ pub struct Definition {
     pub name: String,
     pub description: String,
     pub definition_file: String,
+    pub digest: String,
     pub source_url: Option<String>,
 }
 
@@ -37,6 +67,9 @@ pub struct NewDefinition {
     #[validate(length(max = 300))]
     pub description: String,
 
+    #[validate(custom(function = "validate_digest"))]
+    pub digest: String,
+
     #[validate(url)]
     pub source_url: Option<String>,
 }
@@ -54,6 +87,9 @@ pub struct UpdateDefinition {
 
     #[validate(length(max = 300))]
     pub description: String,
+
+    #[validate(custom(function = "validate_digest"))]
+    pub digest: String,
 
     #[validate(url)]
     pub source_url: Option<String>,
