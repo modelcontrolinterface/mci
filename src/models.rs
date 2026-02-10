1543b9af0551910761cbc1c0584a73f5e5050c10
0x15BA88FF <86390213+0x15BA88FF@users.noreply.github.com>
2026-02-10 04:45:24 -0800

    fix: modify definition model to math schema
    - Rename `enable` to `is_enabled`
    - Add name field to `Definition`, `NewDefinition` and `UpdateDefinition`
    - Tighten validation

--- src/models.rs
+++ src/models.rs
@@ -11,40 +11,50 @@ use validator::Validate;
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Definition {
     pub id: String,
-    pub enabled: bool,
-    pub definition_url: String,
     pub definition_type: String,
-    pub source_url: String,
+    pub is_enabled: bool,
+    pub name: String,
     pub description: String,
+    pub definition_file: String,
+    pub source_url: String,
 }
 
 #[derive(Insertable, Deserialize, Validate, Debug)]
 #[diesel(table_name = definitions)]
 pub struct NewDefinition {
-    #[validate(length(min = 3), regex(path = *NAMESPACE_ID_REGEX))]
+    #[validate(length(min = 3, max=64), regex(path = *NAMESPACE_ID_REGEX))]
     pub id: String,
 
-    #[validate(url)]
-    pub definition_url: String,
-
-    #[validate(length(min = 3), regex(path = *TYPE_IDENTIFIER_REGEX))]
+    #[validate(length(min = 3, max=64), regex(path = *TYPE_IDENTIFIER_REGEX))]
     pub definition_type: String,
 
     #[validate(url)]
-    pub source_url: String,
+    pub definition_file: String,
 
-    #[validate(length(max = 500))]
+    #[validate(length(min = 3, max = 64))]
+    pub name: String,
+
+    #[validate(length(max = 300))]
     pub description: String,
+
+    #[validate(url)]
+    pub source_url: String,
 }
 
 #[derive(AsChangeset, Default, Deserialize, Validate, Debug)]
 #[diesel(table_name = definitions)]
 pub struct UpdateDefinition {
-    pub enabled: Option<bool>,
+    pub is_enabled: Option<bool>,
 
-    #[validate(length(min = 3), regex(path = *TYPE_IDENTIFIER_REGEX))]
+    #[validate(length(min = 3, max=64), regex(path = *TYPE_IDENTIFIER_REGEX))]
     pub definition_type: Option<String>,
 
-    #[validate(length(max = 500))]
-    pub description: Option<String>,
+    #[validate(length(min = 3, max = 64))]
+    pub name: String,
+
+    #[validate(length(max = 300))]
+    pub description: String,
+
+    #[validate(url)]
+    pub source_url: String,
 }
